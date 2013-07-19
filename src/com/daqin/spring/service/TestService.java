59238d9be60e93e43c738f22d99d83b7b37fe580
walala <ql@ql.txgroup.com>
2013-07-19 18:26:58 +0800

    realize login server

--- src/com/daqin/spring/service/TestService.java
+++ src/com/daqin/spring/service/TestService.java
@@ -2,12 +2,16 @@ package com.daqin.spring.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.http.HttpEntity;
@@ -17,6 +21,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+
+import com.daqin.spring.domain.AppProxyConfig;
+import com.daqin.spring.domain.ProxyUserInfoDo;
 
 @Path("/user")
 public class TestService extends BaseService{
@@ -30,14 +37,23 @@ public class TestService extends BaseService{
 	@Path("/login")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public String login(@FormParam("account") String account, @FormParam("password") String password){
+	public String login(@FormParam("account") String account, 
+			@FormParam("password") String password,
+			@Context HttpServletRequest request,
+			@Context HttpServletResponse response){
+		
+		HttpSession session = request.getSession();
+		System.out.println("sessionid: " + session.getId());
+		
 		System.out.println("account: " + account);
 		System.out.println("password: " + password);
+		
 		if("foo@example.com".equals(account) && "hello".equals(password)){
-			LoginDo loginDo = new LoginDo();
-			loginDo.setAccount(account);
-			loginDo.setPassword(password);
-			return newSuccessResult(loginDo);
+			ProxyUserInfoDo user = new ProxyUserInfoDo();
+			user.setRequestCode(AppProxyConfig.USER_LOGIN_SUCCESS);
+			user.setNick("张三");
+			user.setUid(001);
+			return newSuccessResult(user);
 		}
 		 return newErrorResult("login false");
 	}
@@ -45,14 +61,22 @@ public class TestService extends BaseService{
 	@Path("/loginForGet")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String loginForGet(@QueryParam("account") String account, @QueryParam("password") String password){
+	public String loginForGet(@QueryParam("account") String account,
+			@QueryParam("password") String password,
+			@Context HttpServletRequest request,
+			@Context HttpServletResponse response){
+		
+		HttpSession session = request.getSession();
+		System.out.println("sessionid: " + session.getId());
+		
 		System.out.println("account: " + account);
 		System.out.println("password: " + password);
 		if("foo@example.com".equals(account) && "hello".equals(password)){
-			LoginDo loginDo = new LoginDo();
-			loginDo.setAccount(account);
-			loginDo.setPassword(password);
-			return newSuccessResult(loginDo);
+			ProxyUserInfoDo user = new ProxyUserInfoDo();
+			user.setRequestCode(AppProxyConfig.USER_LOGIN_SUCCESS);
+			user.setNick("张三");
+			user.setUid(001);
+			return newSuccessResult(user);
 		}
 		 return newErrorResult("login false");
 	}
@@ -65,9 +89,10 @@ public class TestService extends BaseService{
 	
 	public static void main(String[] args){
 		try{
-			String url = "http://183.194.128.28:7001/maximo/mobile/test/login?username=yaorui&password=ABC";
-			String url1 = "http://183.194.128.28:7001/maximo/mobile/test/desc";
-			String url2 = "http://localhost/zt/rest/user/loginForGet?account=foo@example.com&password=hello";
+//			String url = "http://183.194.128.28:7001/maximo/mobile/test/login?username=yaorui&password=ABC";
+//			String url1 = "http://183.194.128.28:7001/maximo/mobile/test/desc";
+			String url = "http://localhost/zt/rest/user/loginForGet?account=foo@example.com&password=hello";
+			String url1 = "http://localhost/zt/rest/user/loginForGet?account=foo@example.com&password=hello";
 			
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 			HttpGet httpGet = new HttpGet(url);
@@ -116,22 +141,5 @@ public class TestService extends BaseService{
 			ex.printStackTrace();
 		}
 		
-	}
-	
-	private class LoginDo{
-		private String account;
-		private String password;
-		public String getAccount() {
-			return account;
-		}
-		public void setAccount(String account) {
-			this.account = account;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
 	}
 }
